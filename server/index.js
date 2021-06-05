15ff143ad0917b11b9e9989a6ea027880b5389a4
chitaranjan <chitabiswal4@gmail.com>
2021-06-05 16:05:32 +0530

    server added

--- server/index.js
+++ server/index.js
@@ -1,1 +1,26 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const userRoute = require("./routes/user");
+const pinRoute = require("./routes/pin");
 
+dotenv.config();
+
+app.use(express.json());
+
+mongoose
+  .connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+  })
+  .then(() => console.log("MongoDB connected!"))
+  .catch((err) => console.log(err));
+
+app.use("/api/users", userRoute);
+app.use("/api/pins", pinRoute);
+const PORT = process.env.PORT || 8800;
+app.listen(PORT, () => {
+  console.log(`Backend server is running at ${PORT}!`);
+});
